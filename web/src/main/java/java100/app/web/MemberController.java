8d4842c7f2bcf9d5915fe2ce3d2dad153d19a9d3
choinin84 <32053063+choinin84@users.noreply.github.com>
2018-01-22 18:01:57 +0900

    ....

--- web/src/main/java/java100/app/web/MemberController.java
+++ web/src/main/java/java100/app/web/MemberController.java
@@ -17,60 +17,81 @@ import java100.app.service.MemberService;
 public class MemberController {
 
     @Autowired MemberService memberService;
-    
+
     @RequestMapping("list")
     public String list(
-            @RequestParam(value="nm", required=false) String[] names,
+            @RequestParam(value="pn", defaultValue="1") int pageNo,
+            @RequestParam(value="ps", defaultValue="5") int pageSize,
+            @RequestParam(value="words", required=false) String[] words,
             @RequestParam(value="oc", required=false) String orderColumn,
             @RequestParam(value="al", required=false) String align,
-            @RequestParam(value="pn", defaultValue="1") int pageNo,
-            @RequestParam(value="ps", defaultValue="1") int pageSize,
             Model model) throws Exception {
-        if (pageNo < 1 ) {
+
+        // UI 제어와 관련된 코드는 이렇게 페이지 컨트롤러에 두어야 한다.
+        //
+        if (pageNo < 1) {
             pageNo = 1;
         }
-        
+
         if (pageSize < 5 || pageSize > 15) {
             pageSize = 5;
         }
+
         HashMap<String,Object> options = new HashMap<>();
-        options.put("names", names);
+        if (words != null && words[0].length() > 0) {
+            options.put("words", words);
+        }
         options.put("orderColumn", orderColumn);
         options.put("align", align);
+
         int totalCount = memberService.getTotalCount();
         int lastPageNo = totalCount / pageSize;
-        if ((totalCount % pageSize) > 0 ) {
+        if ((totalCount % pageSize) > 0) {
             lastPageNo++;
         }
+
+        // view 컴포넌트가 사용할 값을 Model에 담는다.
         model.addAttribute("pageNo", pageNo);
         model.addAttribute("lastPageNo", lastPageNo);
+
         model.addAttribute("list", memberService.list(pageNo, pageSize, options));
         return "member/list";
-    }
-    
-    @RequestMapping("delete")
-    public String delete(int no) throws Exception {
-        memberService.delete(no);
-        return "redirect:list";
     }
 
     @RequestMapping("{no}")
     public String view(@PathVariable int no, Model model) throws Exception {
+
         model.addAttribute("member", memberService.get(no));
         return "member/view";
+    }
+
+    @RequestMapping("add")
+    public String add(Member member) throws Exception {
+
+        memberService.add(member);
+        return "redirect:list";
+    }
+
+    @RequestMapping("form")
+    public String form() throws Exception {
+        return "member/form";
+
     }
 
     @RequestMapping("update")
     public String update(Member member) throws Exception {
 
-    memberService.update(member);
-    return "redirect:list";
-    
-        }
+        memberService.update(member);
+        return "redirect:list";
     }
 
+    @RequestMapping("delete")
+    public String delete(int no) throws Exception {
 
-
+        memberService.delete(no);
+        return "redirect:list";
+    }
+}
 
 
 
