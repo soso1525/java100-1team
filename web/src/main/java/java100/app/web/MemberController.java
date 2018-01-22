695a3e5e33d4cd3f6927b5e8ebcc99956d5cae18
tjr7788 <sig4213@naver.com>
2018-01-22 17:52:35 +0900

    4

--- web/src/main/java/java100/app/web/MemberController.java
+++ web/src/main/java/java100/app/web/MemberController.java
@@ -61,34 +61,12 @@ public class MemberController {
         return "redirect:list";
     }
     
-}
-
-
-
-
-
-
-
-
-package java100.app.web;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java100.app.domain.Member;
-import java100.app.service.MemberService;
-
-@Controller
-@RequestMapping("/member")
-public class MemberController {
-    @Autowired
-    MemberService memberService;
-
     @RequestMapping("update")
     public String update(Member member) throws Exception {
 
         memberService.update(member);
         return "redirect:list";
     }
+    
 }
+
