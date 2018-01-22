9fcbd1509590af5b277dce391d3f7227b61e9556
choinin84 <32053063+choinin84@users.noreply.github.com>
2018-01-22 17:48:46 +0900

    ...

--- web/src/main/java/java100/app/web/MemberController.java
+++ web/src/main/java/java100/app/web/MemberController.java
@@ -1,12 +1,24 @@
 package java100.app.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java100.app.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
     
+    @Autowired MemberService memberService;
+    
+    @RequestMapping("{no}")
+    public String view(@PathVariable int no, Model model) throws Exception {
+        model.addAttribute("member", memberService.get(no));
+        return "member/view";
+    }
     
 }
 
