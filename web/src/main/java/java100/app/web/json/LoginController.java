c9a47da9332e849334c116b778b1ce628d2f69fb
soso1525 <sandy1525@naver.com>
2018-02-21 10:16:39 +0900

    '..

--- web/src/main/java/java100/app/web/json/LoginController.java
+++ web/src/main/java/java100/app/web/json/LoginController.java
@@ -21,80 +21,63 @@ import java100.app.service.MemberService;
 @RequestMapping("/auth")
 @SessionAttributes("loginUser")
 public class LoginController {
-    
-    @Autowired MemberService memberService;
-    
-    @RequestMapping(value="login", method=RequestMethod.POST)
-    public Object login(
-            String id, 
-            String password,
-            boolean saveEmail,
-            HttpServletResponse response,
-            HttpSession session, /* 세션 객체가 없을 경우 미리 생성할 필요 있다.*/
-            Model model) {
-        
-        Member member = memberService.get(id, password);
-        
-        if (saveEmail) {
-            Cookie cookie = new Cookie("id", id);
-            cookie.setMaxAge(60 * 60 * 24 * 30);
-            response.addCookie(cookie);
-        } else {
-            Cookie cookie = new Cookie("id", "");
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
-        }
-        
-        HashMap<String,Object> result = new HashMap<>();
-        
-        if (member == null) {
-            model.addAttribute("loginUser", null);
-            result.put("status", "fail"); 
-        } else {
-            model.addAttribute("loginUser", member);
-            result.put("status", "success");
-        }
-        return result;
-    }
-    
-    @RequestMapping("logout")
-    public Object logout(HttpSession session, SessionStatus status) {
-        
-        // @SessionAttributes에서 관리하는 세션 데이터를 모두 제거한다.
-        status.setComplete();
-        
-        // HttpSession 객체를 무효화시킨다.
-        session.invalidate();
-        
-        HashMap<String,Object> result = new HashMap<>();
-        result.put("status", "success");
-        return result;
-    }
-    
-    @RequestMapping("loginUser")
-    public Object loginUser(HttpSession session) {
-        
-        Member member = (Member)session.getAttribute("loginUser");
-        
-        HashMap<String,Object> result = new HashMap<>();
-        
-        if (member != null) {
-            result.put("status", "success");
-            result.put("member", member);
-        } else {
-            result.put("status", "fail");
-        }
-            
-        return result;
-    }
+
+	@Autowired
+	MemberService memberService;
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public Object login(String email, String password, boolean saveEmail, HttpServletResponse response,
+			HttpSession session, Model model) {
+
+		Member member = memberService.get(email, password);
+
+		if (saveEmail) {
+			Cookie cookie = new Cookie("email", email);
+			cookie.setMaxAge(60 * 60 * 24 * 30);
+			response.addCookie(cookie);
+		} else {
+			Cookie cookie = new Cookie("email", "");
+			cookie.setMaxAge(0);
+			response.addCookie(cookie);
+		}
+
+		HashMap<String, Object> result = new HashMap<>();
+
+		if (member == null) {
+			model.addAttribute("loginUser", null);
+			result.put("status", "fail");
+		} else {
+			model.addAttribute("loginUser", member);
+			result.put("status", "success");
+		}
+		return result;
+	}
+
+	@RequestMapping("logout")
+	public Object logout(HttpSession session, SessionStatus status) {
+
+		status.setComplete();
+		session.invalidate();
+
+		HashMap<String, Object> result = new HashMap<>();
+		result.put("status", "success");
+		return result;
+	}
+
+	@RequestMapping("loginUser")
+	public Object loginUser(HttpSession session) {
+
+		Member member = (Member) session.getAttribute("loginUser");
+
+		HashMap<String, Object> result = new HashMap<>();
+
+		if (member != null) {
+			result.put("status", "success");
+			result.put("member", member);
+		} else {
+			result.put("status", "fail");
+		}
+
+		return result;
+	}
 }
-
-
-
-
-
-
-
-
-
-
